110b9f5d4552cd64c16adc721c6cfd1018638507
Grégoire <gpierillas@gmail.com>
2020-12-09 10:37:00 +0100

    [Feat] blackboard & shot integrated

--- TP_AI_Project/Assets/Teams/DragonsOfKojima/AIController.cs
+++ TP_AI_Project/Assets/Teams/DragonsOfKojima/AIController.cs
@@ -7,16 +7,24 @@ namespace DragonsOfKojima
 {
     public class AIController : BaseSpaceShipController
     {
+        
+        private Blackboard _blackboard;
+        
         // Start is called before the first frame update
         public override void Initialize(SpaceShip spaceship, GameData data)
         {
+            _blackboard = GetComponent<Blackboard>();
+            _blackboard.Initialize(spaceship, data);
         }
 
-        //public override InputData UpdateInput(SpaceShip spaceship, GameData data)
-        //{
-        //    float thrust = 0.0f;
-        //    float targetOrient = spaceship.Orientation/* + 90.0f*/;
-        //    return new InputData(thrust, targetOrient, false, false, false);
-        //}
+        // public override InputData UpdateInput(SpaceShip spaceship, GameData data)
+        // {
+        //     float thrust = 0.0f;
+        //     float targetOrient = spaceship.Orientation/* + 90.0f*/;
+        //     
+        //     _blackboard.UpdateData(data);
+        //     
+        //     return new InputData(thrust, targetOrient, false, false, false);
+        // }
     }
 }
